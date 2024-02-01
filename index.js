b9bdf1164cfecb0c4fb758b54bafeb2916f61f94
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-01 08:37:40 +0000

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+function main() {
+  const myURLParams = new URL(window.location.toString()).searchParams;
+  const code = myURLParams.get("code");
+  if (code !== null) {
+    const appHTML = document.getElementById("app");
+    const hElement = document.createElement("h1");
+    hElement.innerText = code;
+    appHTML.appendChild(hElement);
+  }
+}
