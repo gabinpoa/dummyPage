06033e73c404ca88b051d35ed6c5e4d52e86d17e
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-02 04:18:29 +0000

    add console log test and try to fix routing

--- src/authcode.js
+++ src/authcode.js
@@ -2,10 +2,12 @@ function main() {
   const myURLParams = new URL(window.location.toString()).searchParams;
   const code = myURLParams.get("code");
   if (code !== null) {
+    console.log("has");
     const appHTML = document.getElementById("app");
     const hElement = document.createElement("h1");
     hElement.innerText = code;
     appHTML.appendChild(hElement);
   }
+  console.log("it runned");
 }
 main();
