9150d99bcc7e2e832ed877c05606d6948cb83b60
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-02 20:29:46 +0000

    make link more dynamic

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function main() {
-  const loginLink = document.createElement("a");
+  const client_id = "396942749405934";
+  const redirect_uri = "https://gabinpoa.github.io/dummyPage/static/index.html";
+  const scope =
+    "user_profile,instagram_basic,pages_read_engagement,pages_show_list";
+
   const url = new URL(
-    "https://api.instagram.com/oauth/authorize?client_id=396942749405934&redirect_uri=https://gabinpoa.github.io/dummyPage/static/index.html&scope=user_profile&response_type=code",
+    `https://api.instagram.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code`,
   );
+
+  const loginLink = document.createElement("a");
   loginLink.innerText = "Login";
   loginLink.href = url.toString();
 
-  const app = document.getElementById("app");
-  app.appendChild(loginLink);
+  document.getElementById("app").appendChild(loginLink);
 }
 main();
