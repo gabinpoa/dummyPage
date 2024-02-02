b0096e412fb9408fe0bd4538555521233a8bf5b4
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-02 04:39:28 +0000

    remove invalid openid parameter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function main() {
   const loginLink = document.createElement("a");
   const url = new URL(
-    "https://api.instagram.com/oauth/authorize?client_id=396942749405934&redirect_uri=https://gabinpoa.github.io/dummyPage/static/index.html&scope=user_profile,openid&response_type=code",
+    "https://api.instagram.com/oauth/authorize?client_id=396942749405934&redirect_uri=https://gabinpoa.github.io/dummyPage/static/index.html&scope=user_profile&response_type=code",
   );
   loginLink.innerText = "Login";
   loginLink.href = url.toString();
