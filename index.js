12b18df81abdf944773ecdf49529f8901b3e3600
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-03 04:29:55 +0000

    Add customize clientId

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-function main() {
-  const client_id = "396942749405934";
-  const redirect_uri = "https://gabinpoa.github.io/dummyPage/static/index.html";
-  const scope =
-    "user_profile,instagram_basic,pages_read_engagement,pages_show_list";
+function appendToApp(element) {
+  document.getElementById("app").appendChild(element);
+}
 
-  const url = new URL(
-    `https://api.instagram.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code`,
-  );
-
+function getATagElement(url) {
   const loginLink = document.createElement("a");
   loginLink.innerText = "Login";
   loginLink.href = url.toString();
-
-  document.getElementById("app").appendChild(loginLink);
+  return loginLink;
 }
+
+function getUrl({ client_id, redirect_uri, scope }) {
+  return new URL(
+    `https://api.instagram.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code`,
+  );
+}
+
+function main() {
+  document.getElementById("authbutton").addEventListener("click", (event) => {
+    event.preventDefault();
+    const options = {
+      client_id: document.getElementById("client_id").value,
+      redirect_uri: "https://gabinpoa.github.io/dummyPage/static/index.html",
+      scope:
+        "user_profile,instagram_basic,pages_read_engagement,pages_show_list",
+    };
+
+    const url = getUrl(options);
+    const aElement = getATagElement(url);
+    appendToApp(aElement);
+  });
+}
+
 main();
